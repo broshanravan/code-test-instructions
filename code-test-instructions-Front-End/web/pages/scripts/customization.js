8c49b68386f4650c5dc6ad88933c202ace63bb2d
Behrooz Roshanravan <broshanravan@hotmail.com>
2025-12-07 08:08:49 +0000

    accessing ARIs from the front end module

---
+++ code-test-instructions-Front-End/web/pages/scripts/customization.js
@@ -0,0 +1,39 @@
+const request = require('request')
+const axios = require('axios');
+
+exports.customizeURL = (original_URL,customized_URL) =>{
+    const api_url = `http://localhost:8080/customizeTheURL?originalURLStr=${original_URL}&newCustomizedUrl=${customized_URL}`;
+    request.post({url:api_url},(error,response) =>{
+        console.log(response.body);
+    });
+
+}
+exports.getOriginalURL = async ( customized_URL) => {
+
+    const api_url = `http://localhost:8080/getOriginalURL?newCustomizedUrl=${customized_URL}`;
+
+    request({url:api_url},(error,response) =>{
+        console.log(response.body);
+    });
+
+
+
+}
+exports.deleteURLRecord = (customized_URL) =>{
+    const api_url = `http://localhost:8080/deleteUrlRecord?customizedUrl=${customized_URL}`;
+    axios
+        .delete(api_url)
+        .then((res) => {
+            // Handle the response
+            console.log('Status code:', res.status); // Print the status code
+            console.log('Headers:', res.headers); // Print the headers
+            console.log('Data:', res.data); // Print the data
+        })
+        .catch((err) => {
+            // Handle any errors
+            console.error('Error:', err.message); // Print the error message
+        });
+}
+
+//this.getOriginalURL("google1")
+this.deleteURLRecord("mycnn")
